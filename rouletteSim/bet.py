6fb42e0794c92cdb77f4918dfcf8aa33c8df1169
David Garcia <49959037+garciadavid2000@users.noreply.github.com>
2023-10-09 17:26:39 -0400

    Change variable names in bet.py

--- rouletteSim/bet.py
+++ rouletteSim/bet.py
@@ -1,20 +1,14 @@
-
-
 class Bet:
-    betVals = []
-    tag = ""
-    payout = 0
-    betAmount = 0
 
-    def init(self, bv, t,  p, ba):
-        self.betVals = bv
-        self.tag = t
-        self.payout = p
-        self.betAmount = ba
+    def __init__(self, betNumbers=[], betType: str="",  payout: int=0, betAmount: int=0):
+        self.betVals = betNumbers
+        self.betType = betType
+        self.payout = payout
+        self.betAmount = betAmount
 
-    def str(self):
+    def __str__(self):
         return f"bet vals: {self.betVals}, payout: {self.payout}, bet amount: {self.betAmount}"
 
-    def getPayout(self, rv):
+    def getPayout(self, rouletteOutcome: str) -> int:
         payoutValue = self.payout*self.betAmount
-        return payoutValue + self.betAmount if rv in self.betVals else -self.betAmount
+        return payoutValue + self.betAmount if rouletteOutcome in self.betVals else -self.betAmount
