4da9ebc8c2929ed4929ada255682b70869b9e2cb
David Garcia <49959037+garciadavid2000@users.noreply.github.com>
2023-10-09 18:09:40 -0400

    Update bet.py
    Create getters and setters as well as helper functions
    
    Co-authored-by: Peterk3275761769 using liveshare <Peterk3275761769@users.noreply.github.com>

--- rouletteSim/bet.py
+++ rouletteSim/bet.py
@@ -1,14 +1,40 @@
 class Bet:
 
     def __init__(self, betNumbers=[], betType: str="",  payout: int=0, betAmount: int=0):
-        self.betVals = betNumbers
+        self.betNumbers = betNumbers
         self.betType = betType
         self.payout = payout
         self.betAmount = betAmount
 
     def __str__(self):
-        return f"bet vals: {self.betVals}, payout: {self.payout}, bet amount: {self.betAmount}"
+        return f"bet numbers: {self.betNumbers}, payout: {self.payout} : 1, bet amount: ${self.betAmount}"
 
+
+    # Get payout of Bet given the outcome of the roulette wheel
     def getPayout(self, rouletteOutcome: str) -> int:
         payoutValue = self.payout*self.betAmount
-        return payoutValue + self.betAmount if rouletteOutcome in self.betVals else -self.betAmount
+        return payoutValue + self.betAmount if rouletteOutcome in self.betNumbers else -self.betAmount
+
+
+    # Check if a Bet covers a specific outcome
+    def coversValue(self, rouletteValue: str)-> bool:
+        return True if rouletteValue in self.betNumbers else False
+
+    # Getters
+    def getBetNumbers(self):
+        return self.betNumbers
+
+    def getBetType(self) -> str:
+        return self.betType
+
+    def getPayout(self) -> int:
+        return self.payout
+
+    def getBetAmount(self) -> int:
+        return self.betAmount
+    
+    # Setters
+    def setBetAmount(self, betAmount):
+        self.betAmount = betAmount\
+
+    #wawa
